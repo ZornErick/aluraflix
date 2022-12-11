6511cb36a8fff0cb2a44d1e8b30f5cfb926ff09d
ZornErick <erick.nogueira.zorn@gmail.com>
2022-12-11 19:01:23 -0300

    Implementação do Swagger

--- src/main/java/br/com/alura/aluraflix/AluraflixApplication.java
+++ src/main/java/br/com/alura/aluraflix/AluraflixApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-//@EnableSwagger2
+@EnableSwagger2
 public class AluraflixApplication {
 
     public static void main(String[] args) {
