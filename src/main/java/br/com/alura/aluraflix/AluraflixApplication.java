42e13ae3e3943fdc7fa3f3e1d549ea16711b31ae
ZornErick <erick.nogueira.zorn@gmail.com>
2022-12-11 19:00:31 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/alura/aluraflix/AluraflixApplication.java
@@ -0,0 +1,15 @@
+package br.com.alura.aluraflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+//@EnableSwagger2
+public class AluraflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AluraflixApplication.class, args);
+    }
+
+}
---
+++ src/main/java/br/com/alura/aluraflix/ServletInitializer.java
@@ -0,0 +1,13 @@
+package br.com.alura.aluraflix;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AluraflixApplication.class);
+    }
+
+}
